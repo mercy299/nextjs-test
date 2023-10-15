215ea65fd6640a18042d4c264afb4b3ff0ca9332
mercy299 <mercysam299@gmail.com>
2023-10-15 22:15:26 +0100

    changed page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,6 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-   <h1 className='text-danger'>Welcome bisssshhhh </h1>
+   <h1 className='text-danger'>Welcome</h1>
   )
 }
